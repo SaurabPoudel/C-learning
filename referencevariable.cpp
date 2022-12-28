463817574bfb46e64af7b87f98b8d6a9801e02ef
Saurab Poudel <poudelsaurab20@gmail.com>
2022-12-28 12:18:03 +0545

    Day 1

---
+++ referencevariable.cpp
@@ -0,0 +1,12 @@
+// **************************Reference variable**********************************
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x = 455;
+    float & y = x;
+    cout << x<<endl;
+    cout << y << endl;
+    return 0;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    float b = 45.46;
+    cout << "The value of b is "<<int(b)<<endl;
+    cout << "The value of b is "<<(int)b;
+
+}
